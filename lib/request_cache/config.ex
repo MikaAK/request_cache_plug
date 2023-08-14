858465c42000e8cc8a5686494e84e19e1fa881fc
Mika Kalathil <mika@kalathil.me>
2023-08-14 15:16:47 -0400

    Make sure you can set enabled? to false

--- lib/request_cache/config.ex
+++ lib/request_cache/config.ex
@@ -4,7 +4,7 @@ defmodule RequestCache.Config do
   @app :request_cache_plug
 
   def verbose? do
-    Application.get_env(@app, :verbose?) || false
+    !!Application.get_env(@app, :verbose?)
   end
 
   def graphql_paths do
@@ -24,7 +24,7 @@ defmodule RequestCache.Config do
   end
 
   def enabled? do
-    Application.get_env(@app, :enabled?) || true
+    !!Application.get_env(@app, :enabled?)
   end
 
   def default_concache_opts do
